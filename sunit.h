7011b959a1a1a14f9b8a2de7320343265cff84a8
Jasper Shovelton <Beanie.github@proton.me>
2023-07-03 22:08:10 +0100

    add some colour ;)

--- sunit.h
+++ sunit.h
@@ -4,14 +4,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define bold		"\033[1m"
+#define red		"\033[31m"
+#define green		"\033[32m"
+#define reset		"\033[m"
+
 #define ASSERT(a)\
 do {\
 	if (a) {\
-		fprintf(stderr, "PASSED: " __FILE__ ", %d: \'" #a "\' "\
-				"expected true; got true\n", __LINE__);\
+		fprintf(stderr, green bold "PASSED: " reset __FILE__ ", %d: \'"\
+				#a "\' expected true; got true\n", __LINE__);\
 	} else {\
-		fprintf(stderr, "FAILED: " __FILE__ ", %d: \'" #a "\' "\
-				"expected true; got false\n", __LINE__);\
+		fprintf(stderr, red bold "FAILED: " reset __FILE__ ", %d: \'"\
+				#a "\' expected true; got false\n", __LINE__);\
 		exit(1);\
 	}\
 } while (0)
