07391eb0751943b7db856fcac256e433fe535527
Jasper Shovelton <Beanie.github@proton.me>
2023-07-04 11:04:09 +0100

    remove repeated code in macros thanks to a couple more macros

--- sunit.h
+++ sunit.h
@@ -8,87 +8,68 @@
 #define RED             "\033[31m"
 #define GREEN           "\033[32m"
 #define RESET           "\033[m"
+#define MSG_PASS(a, msg) \
+	fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'" #a\
+			"\' " msg "\n", __LINE__)
+#define MSG_FAIL(a, msg) \
+	fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'" #a\
+			"\' " msg "\n", __LINE__)
+
 
 #define ASSERT(a)\
 do\
 	if (a)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected true\n", __LINE__);\
+		MSG_PASS(a, "expected true");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected true\n", __LINE__);\
+		MSG_FAIL(a, "expected true");\
 while (0)
 
 #define ASSERT_EQ(a, b)\
 do\
 	if (a == b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to equal \'" #b "\'\n",\
-				__LINE__);\
+		MSG_PASS(a, "expected to equal \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to equal \'" #b "\'\n",\
-				__LINE__);\
+		MSG_FAIL(a, "expected to equal \'" #b "\'");\
 while (0)
 
 #define ASSERT_NEQ(a, b)\
 do\
 	if (a != b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected not to equal \'" #b "\'\n",\
-				__LINE__);\
+		MSG_PASS(a, "expected not to equal \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected not to equal \'" #b "\'\n",\
-				__LINE__);\
+		MSG_FAIL(a, "expected not to equal \'" #b "\'");\
 while (0)
 
 #define ASSERT_GT(a, b)\
 do\
 	if (a > b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be greater than \'" #b\
-				"\'\n", __LINE__);\
+		MSG_PASS(a, "expected to be greater than \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be greater than \'" #b\
-				"\'\n", __LINE__);\
+		MSG_FAIL(a, "expected to be greater than \'" #b "\'");\
 while (0)
 
 #define ASSERT_LT(a, b)\
 do\
 	if (a < b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be less than \'" #b "\'\n",\
-				__LINE__);\
+		MSG_PASS(a, "expected to be less than \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be less than \'" #b "\'\n",\
-				__LINE__);\
+		MSG_FAIL(a, "expected to be less than \'" #b "\'");\
 while (0)
 
 #define ASSERT_GEQ(a, b)\
 do\
 	if (a >= b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be greater than or equal "\
-				"to \'" #b "\'\n", __LINE__);\
+		MSG_PASS(a, "expected to be greater than or equal to \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be greater than or equal "\
-				"to \'" #b "\'\n", __LINE__);\
+		MSG_FAIL(a, "expected to be greater than or equal to \'" #b "\'");\
 while (0)
 
 #define ASSERT_LEQ(a, b)\
 do\
 	if (a <= b)\
-		fprintf(stderr, GREEN BOLD "PASSED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be less than or equal to "\
-				"\'" #b "\'\n", __LINE__);\
+		MSG_PASS(a, "expected to be less than or equal to \'" #b "\'");\
 	else\
-		fprintf(stderr, RED BOLD "FAILED: " RESET __FILE__ ", %d: \'"\
-				#a "\' expected to be less than or equal to "\
-				"\'" #b "\'\n", __LINE__);\
+		MSG_FAIL(a, "expected to be less than or equal to \'" #b "\'");\
 while (0)
 
 #endif
